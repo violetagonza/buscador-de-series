377375d04042ac11e7d81c2472ad95b126c10564
violetagonza <violetagonzalezcalleja@gmail.com>
2020-03-20 12:04:01 +0100

    Add one of the shows in the favs array to favs list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -26,17 +26,18 @@ function getData() {
 function paintResults() {
   const list = document.querySelector('.js-list');
   list.innerHTML = '';
-  let HTMLcode = '';
+  let HTMLSearchcode = '';
   for (let i = 0; i < searchResults.length; i++) {
-    HTMLcode += `<li class="card--normal js-card"> ${searchResults[i].show.name}`;
+    HTMLSearchcode += `<li class="card--normal js-card"> ${searchResults[i].show.name}`;
     if (searchResults[i].show.image === null) {
-      HTMLcode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/? text=TV" alt="${searchResults[i].show.name}"></li>`;
+      HTMLSearchcode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/? text=TV" alt="${searchResults[i].show.name}"></li>`;
     } else {
-      HTMLcode += `<img src="${searchResults[i].show.image.medium}" alt="${searchResults[i].show.name}"></li>`;
+      HTMLSearchcode += `<img src="${searchResults[i].show.image.medium}" alt="${searchResults[i].show.name}"></li>`;
     }
   }
-  list.innerHTML = HTMLcode;
+  list.innerHTML = HTMLSearchcode;
   listenCards();
+  console.log(searchResults);
 }
 
 //Escucho al botón
@@ -44,19 +45,46 @@ btn.addEventListener('click', handlebtn);
 
 //Favs
 
-//Escucho las tarjetas
-function listenCards() {
-  // Recojo tarjeta (temporalmete imagen porque no me genera el div)
-  const cards = document.querySelectorAll('.js-card');
-
-  // Escucho tarjeta (temporalmete imagen porque no me genera el div)
-  for (const card of cards) {
-    card.addEventListener('click', handleCard);
+//Creo array para favs
+let favs = [];
+//Añado al array el clickado
+function addShowToFavs() {
+  favs.push(searchResults[0]);
+  console.log(favs);
+}
+function paintFavs() {
+  const favList = document.querySelector('.js-fav-list');
+  let HTMLFavsCode;
+  HTMLFavsCode = `<li>${favs[0].show.name}`;
+  if (favs[0].show.image === null) {
+    HTMLFavsCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/? text=TV" alt="${searchResults[i].show.name}"></li>`;
+  } else {
+    HTMLFavsCode += `<img src="${favs[0].show.image.medium}" alt="${favs[0].show.name}"></li>`;
   }
+  favList.innerHTML = HTMLFavsCode;
 }
 
 // //Handle de la tarjeta
-function handleCard() {
+function handleCard(ev) {
+  //Cambio de color cuando hago click
+  if (ev.currentTarget.classList.contains('card--normal')) {
+    ev.currentTarget.classList.remove('card--normal');
+    ev.currentTarget.classList.add('card--fav');
+  } else if (ev.currentTarget.classList.contains('card--fav')) {
+    ev.currentTarget.classList.remove('card--fav');
+    ev.currentTarget.classList.add('card--normal');
+  }
+  addShowToFavs();
+  paintFavs();
   console.log('me han clickado');
-  //   paintFavs();
+}
+
+//Escucho las tarjetas
+function listenCards() {
+  // Recojo tarjeta
+  const cards = document.querySelectorAll('.js-card');
+  // Escucho tarjeta
+  for (const card of cards) {
+    card.addEventListener('click', handleCard);
+  }
 }
