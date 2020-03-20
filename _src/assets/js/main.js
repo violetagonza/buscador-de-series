d7e89a7a40cbc10d0461b8a006e20f6b86e623ea
violetagonza <violetagonzalezcalleja@gmail.com>
2020-03-20 10:05:16 +0100

    Fix the double click problem with the search button

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,27 +3,23 @@
 const btn = document.querySelector('.js-btn');
 const input = document.querySelector('.js-input');
 
-// Declaro un array para meter los resultados de búsqueda
-let searchResults = [];
-
 //Handle del btn
 function handlebtn() {
-  //   event.preventDefault();
+  event.preventDefault();
   getData();
 }
 
+// Declaro un array para meter los resultados de búsqueda
+let searchResults = [];
 //Fetch al servidor
 function getData() {
   fetch(`http://api.tvmaze.com/search/shows?q=${input.value}`)
     .then(response => response.json())
-    .then(data => {
-      return data;
-    })
-    //Meto la info en un array
+    //Meto la info en el array searchResults
     .then(results => {
       searchResults = results;
+      paintResults();
     });
-  paintResults();
 }
 
 //Pinto los resultados en el DOM
@@ -31,14 +27,18 @@ function paintResults() {
   const list = document.querySelector('.js-list');
   list.innerHTML = '';
   for (let i = 0; i < searchResults.length; i++) {
+    list.innerHTML += '<div>';
     list.innerHTML += `<li> ${searchResults[i].show.name}</li>`;
     if (searchResults[i].show.image === null) {
       list.innerHTML += `<img src="https://via.placeholder.com/210x295/ffffff/666666/? text=TV" alt="${searchResults[i].show.name}">`;
     } else {
       list.innerHTML += `<img src="${searchResults[i].show.image.medium}" alt="${searchResults[i].show.name}">`;
     }
+    list.innerHTML += '</div>';
   }
 }
 
 //Escucho al botón
 btn.addEventListener('click', handlebtn);
+
+// escucho tarjeta (temporalmete imagen porque no me genera el div)
